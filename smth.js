9816d830ca24d221e94a13de52a0fd9d14881555
microwave694 <125757696+microwave694@users.noreply.github.com>
2023-12-01 23:09:20 +0100

    Add files via upload

--- smth.js
+++ smth.js
@@ -1,1 +1,16 @@
-h
+function load1(){
+	document.getElementsByClassName('textarea')[0].innerHTML = "<h2>This section covers everything up until AGHGS30!</h2><br><br>Full early game guide:<br><a href='https://docs.google.com/document/d/1T8WXqSAovEo8_5RtoWslPwyVl9N8TYR-Kep8VrN296s/'>Early game guide</a> Last updated: V0.8.1<br><br>Frequently Asked Questions (FAQ):	<br><br>When do I galactic?<br>Usually just getting more stars than your last one is better until you have hit AGHGS 18+. By then you should have a good feel for when you would want a galactic. If you still struggle with knowing, then potentially set a goal for yourself on how many stars you want. Don't stay too long for your first 3 galactics, your first one should be at 10, the next two should be done quickly to unlock the ability to AGH.<br><br>Where do I spend stars?<br>Stars are best spent in the speed tree first on charge upgrades due to the main issue before AGHGS milestones being charge. Progression mainly serves as a boost to getting Astral and making Autocut better. Automation is good for QOL and takes less than 100k stars to get all the Factory+ Automation which helps a lot. Overall, its best to go for Speed then Automation then Progression.<br><br>When do I get the Star Accumulator. <br>Ignore it until you have gotten steel generation. It takes way too long without steel generation as your steel eventually softcaps before the accumulator cost. Steel generation allows you to sit on the max amount for a while to generate the steel to make up the deficit as well as maxed faster foundry allows for a quick max foundry bonus.<br><br>How do I get AGH milestones fast?<br><br>My recommendation is after your 3rd galactic (Doing an AGH milestone on your 3rd does not count) that you go for AGH 19 for more stars, then a star run (A run getting full grasshops and grass skips trying to get a lot of stars) AGH 13, star run, AGH 7, then AGHGS 6 (AGHGS 6 is easier than AGH 7 because of accomplishments no longer resetting on GH/Steelie)."
+}
+function load2(){
+	document.getElementsByClassName('textarea')[0].innerHTML = "<h2>This section covers everything up until your 1st Supernova!</h2><br><br>Constellation guide:<br><a href='https://docs.google.com/document/d/12ndIb4bepgpUlKYK3qsIHMPDu2M-1BlU5GQMDQ9YuQA/edit#heading=h.p42wg1fjvv0x'>Constellation guide</a> Last updated: V0.8.1 (partially)"
+}
+
+function loadbutton1(){
+	document.getElementById("buttonrow1").hidden = false
+	document.getElementById("buttonrow2").hidden = true
+}
+
+function loadbutton2(){
+	document.getElementById("buttonrow1").hidden = true
+	document.getElementById("buttonrow2").hidden = false
+}
